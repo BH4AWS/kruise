677e25edcbbc1786eb152e05e5f4335bc50f9eb5
酒祝 <jiuzhu.wsy@alibaba-inc.com>
2021-01-12 15:29:03 +0800

    Add account-manager for daemon

--- pkg/daemon/util/account_manager.go
+++ pkg/daemon/util/account_manager.go
@@ -19,9 +19,17 @@ package util
 import (
 	"encoding/base64"
 	"encoding/json"
+	"fmt"
+	"math/rand"
+	"strings"
+	"sync"
+	"time"
 
 	dockertypes "github.com/docker/docker/api/types"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	clientset "k8s.io/client-go/kubernetes"
+	"k8s.io/klog"
 )
 
 type AuthInfo struct {
@@ -44,5 +52,82 @@ type ImagePullAccountManager interface {
 
 // NewImagePullAccountManager returns an ImagePullAccountManager, defaults to be nil
 func NewImagePullAccountManager(kubeClient clientset.Interface) ImagePullAccountManager {
-	return nil
+	return &imagePullAccountManager{
+		kubeClient:    kubeClient,
+		authInfoCache: make(map[string]authInfoCacheItem),
+	}
+}
+
+const (
+	internalRegistrySuffix = "docker.alibaba-inc.com"
+	internalRegistryUser   = "aone"
+	internalRegistryUsage  = "ali-registry-user-account"
+)
+
+type authInfoCacheItem struct {
+	deadline time.Time
+	authInfo *AuthInfo
+}
+
+func (c authInfoCacheItem) isExpired() bool {
+	return time.Now().After(c.deadline)
+}
+
+type imagePullAccountManager struct {
+	sync.Mutex
+	kubeClient    clientset.Interface
+	authInfoCache map[string]authInfoCacheItem
+}
+
+func (i *imagePullAccountManager) GetAccountInfo(repo string) (*AuthInfo, error) {
+	registry := ParseRegistry(repo)
+	isInternalDomain := strings.HasSuffix(registry, internalRegistrySuffix)
+	if !isInternalDomain {
+		return nil, nil
+	}
+
+	i.Lock()
+	defer i.Unlock()
+	if cache, ok := i.authInfoCache[registry]; ok {
+		if !cache.isExpired() {
+			return cache.authInfo, nil
+		}
+	}
+
+	// 1. 从kube-system下找到aone账号（兼容alipodlifecyclehook逻辑）
+	if info, err := i.getAuthFromKubeSystem(); err != nil {
+		klog.Warningf("Failed to get secret in kube-system: %v", err)
+	} else if info != nil {
+		// renew cache in 5~10 minutes
+		interval := time.Duration(rand.Int31n(6)+5) * time.Minute
+		item := authInfoCacheItem{
+			deadline: time.Now().Add(interval),
+			authInfo: info,
+		}
+		i.authInfoCache[registry] = item
+		return info, nil
+	}
+
+	return nil, fmt.Errorf("not found auth info for %v", repo)
+}
+
+func (i *imagePullAccountManager) getAuthFromKubeSystem() (*AuthInfo, error) {
+	opts := metav1.ListOptions{
+		LabelSelector:   fmt.Sprintf("usage=%s,username=%s", internalRegistryUsage, internalRegistryUser),
+		ResourceVersion: "0",
+	}
+	secretList, err := i.kubeClient.CoreV1().Secrets("kube-system").List(opts)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, s := range secretList.Items {
+		if s.Type == v1.SecretTypeOpaque {
+			return &AuthInfo{
+				Username: string(s.Data["username"]),
+				Password: string(s.Data["password"]),
+			}, nil
+		}
+	}
+	return nil, fmt.Errorf("not found aone registry secrets")
 }
