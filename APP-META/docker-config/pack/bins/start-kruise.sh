d8920748e27d0b0d4be4e11a159d16cee7ac4057
酒祝 <jiuzhu.wsy@alibaba-inc.com>
2021-02-18 15:33:04 +0800

    enable acj,bcj,ud

--- APP-META/docker-config/pack/bins/start-kruise.sh
+++ APP-META/docker-config/pack/bins/start-kruise.sh
@@ -22,34 +22,34 @@ crond -m/dev/null &
 #    exit 1
 #fi
 
-args="-rest-config-burst=500 -rest-config-qps=300 -logtostderr -enable-leader-election -leader-election-namespace=kube-system -enable-pprof"
+args="--rest-config-burst=500 --rest-config-qps=300 --logtostderr --enable-leader-election --leader-election-namespace=kube-system --enable-pprof"
 
 if [ ! -z "$METRICS_PORT" ]; then
-	args="$args -metrics-addr=0.0.0.0:$METRICS_PORT"
+	args="$args --metrics-addr=0.0.0.0:$METRICS_PORT"
 fi
 
 if [ ! -z "$HEALTH_PORT" ]; then
-	args="$args -health-probe-addr=0.0.0.0:$HEALTH_PORT"
+	args="$args --health-probe-addr=0.0.0.0:$HEALTH_PORT"
 fi
 
 if [ ! -z "$PPROF_PORT" ]; then
-	args="$args -pprof-addr=0.0.0.0:$PPROF_PORT"
+	args="$args --pprof-addr=0.0.0.0:$PPROF_PORT"
 fi
 
 if [ -z "$LOG_LEVEL" ]; then
 	LOG_LEVEL="5"
 fi
-args="$args -v=$LOG_LEVEL"
+args="$args --v=$LOG_LEVEL"
 
 if [ -z "$RATE_LIMITER_MAX_DELAY" ]; then
   RATE_LIMITER_MAX_DELAY="10m"
 fi
-args="$args -rate-limiter-max-delay=$RATE_LIMITER_MAX_DELAY"
+args="$args --rate-limiter-max-delay=$RATE_LIMITER_MAX_DELAY"
 
 if [ -z "$CLONESET_WORKERS" ]; then
 	CLONESET_WORKERS="20"
 fi
-args="$args -cloneset-workers=$CLONESET_WORKERS"
+args="$args --cloneset-workers=$CLONESET_WORKERS"
 
 pidof kruise-manager || {
   cd /home/admin/kruise
