e2fc2757f76b3c350e3fd9aab59b905e361ce343
酒祝 <jiuzhu.wsy@alibaba-inc.com>
2021-04-20 19:32:27 +0800

    add dockerfile for aarch64

--- APP-META/docker-config/build.sh
+++ APP-META/docker-config/build.sh
@@ -8,15 +8,24 @@ go version
 go env
 
 # build
-#make manifests 1>&2
 CGO_ENABLED=0 go build -mod=vendor -a -o APP-META/docker-config/pack/bins/kruise-manager main.go
 CGO_ENABLED=0 go build -mod=vendor -a -o APP-META/docker-config/pack/bins/kruise-daemon ./cmd/daemon
 
 # package
-wget -P APP-META/docker-config/pack/bins/ http://iops.oss-cn-hangzhou-zmf.aliyuncs.com/kubectl/1.14/kubectl
+arch=$(uname -m)
+if [[ $arch == x86_64 ]]; then
+  wget -P APP-META/docker-config/pack/bins/ http://iops.oss-cn-hangzhou-zmf.aliyuncs.com/kubectl/1.18/amd64/kubectl
+elif  [[ $arch == aarch64 ]]; then
+  wget -P APP-META/docker-config/pack/bins/ http://iops.oss-cn-hangzhou-zmf.aliyuncs.com/kubectl/1.18/arm64/kubectl
+else
+  echo "unsupported arch: ${arch}"
+  exit 1
+fi
 chmod +x APP-META/docker-config/pack/bins/kubectl
-cp -r config/crd/bases/apps.kruise.io_statefulsets.yaml APP-META/docker-config/pack/crds/
-cp -r config/crd/bases/apps.kruise.io_daemonsets.yaml APP-META/docker-config/pack/crds/
-cp -r config/crd/bases/apps.kruise.io_clonesets.yaml APP-META/docker-config/pack/crds/
-cp -r config/crd/bases/apps.kruise.io_imagepulljobs.yaml APP-META/docker-config/pack/crds/
-cp -r config/crd/bases/apps.kruise.io_nodeimages.yaml APP-META/docker-config/pack/crds/
+
+# update the image tag
+GIT_VERSION_SHORT=$(git rev-parse --verify HEAD --short)
+ARCH=$(uname -m)
+DATE=$(date '+%Y%m%d%H%M')
+version=${CODE_BRANCH}_${ARCH}_${GIT_VERSION_SHORT}_${DATE}
+sed -i "s/docker.tag=.*/docker.tag=${version}/" kruise.release
